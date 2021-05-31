741df8a7ad3aaf28d0f24818112b91bde2b731e3
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-05-31 00:50:05 -0300

    Second solution to question1

--- src/question1.py
+++ src/question1.py
@@ -18,35 +18,37 @@ class Contract:
 
 def get_top_N_open_contracts(open_contracts: list[Contract], renegotiated_contracts: list[int], top_n: int) -> list[int]:
         
-    contracts_not_renegotiated = [(contract.get_debt(), contract.get_id()) for contract in open_contracts 
-                                    if contract.get_id() not in renegotiated_contracts]
+    renegotiated_set = {*renegotiated_contracts}
 
-    # Highest debtor to smallest debtor
-    contracts_not_renegotiated.sort(reverse=True)
+    contracts_not_renegotiated = [(contract.get_debt(), contract.get_id(), ) for contract in open_contracts 
+                                    if contract.get_id() not in renegotiated_set]
 
- 
-    max_contratcs_to_return = top_n if len(contracts_not_renegotiated) >= top_n else len(contracts_not_renegotiated) 
+    number_of_contracts_to_return = top_n if len(contracts_not_renegotiated) >= top_n else len(contracts_not_renegotiated) 
 
-    top_n_contracts_not_renegotiated = [id  for debt, id in contracts_not_renegotiated[:max_contratcs_to_return] ]
- 
-    return top_n_contracts_not_renegotiated 
+    ordered_contracts = sorted(contracts_not_renegotiated,reverse=True)[:number_of_contracts_to_return]
+
+    top_n_contracts = [id for debt, id in ordered_contracts]
+
+
+    return top_n_contracts
 
 
 
 
 contracts_list = [
     Contract(1, 10),
-    Contract(2, 2),
+    Contract(2, 100),
     Contract(3, 3),
     Contract(4, 4),
-    Contract(5, 5)
+    Contract(5, 20)
 ]
-renegotiated = [3]
+renegotiated = [3, 4]
+
 top_n = 3
 
 actual_open_contracts = get_top_N_open_contracts(open_contracts=contracts_list, renegotiated_contracts=renegotiated, top_n=top_n)
 
 
 print(actual_open_contracts)
-expected_open_contracts = [1, 5, 4,]
+expected_open_contracts = [2, 5, 1]
 assert expected_open_contracts == actual_open_contracts
