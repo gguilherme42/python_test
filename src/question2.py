31a3cc60badb62b431c3ca7ec4b5e57a7d43ffdb
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-05-31 12:57:49 -0300

    First solution to question 2

--- src/question2.py
+++ src/question2.py
@@ -1,3 +1,43 @@
 class Orders:
-    def combine_orders(self, requests, n_max):
-        pass
+    def combine_orders(self, requests: list[int], n_max: int) -> int:
+        requests_copy = requests[:]
+        trips_pair = []
+        while requests_copy:
+          
+            highest_value = max(requests_copy)
+            
+            smallest_value = min(requests_copy)
+            
+            
+            if len(requests_copy) == 1:
+                trips_pair.append((highest_value, 0))
+
+            elif (highest_value + smallest_value) <= n_max:
+                trips_pair.append((highest_value, smallest_value))
+            
+            else: 
+                trips_pair += [(highest_value, 0), (smallest_value, 0)]
+            
+            if len(requests_copy) == 1:
+                requests_copy.remove(highest_value)
+            else:
+                requests_copy.remove(highest_value)
+                requests_copy.remove(smallest_value)
+            
+        minimum_number_of_trips = len(trips_pair)
+        
+        return minimum_number_of_trips
+
+            
+
+
+        
+
+
+orders = [90, 40, 70, 80, 36, 20, 15, 3]
+n_max = 100
+expected_orders = 4
+
+how_many = Orders().combine_orders(orders, n_max)
+
+assert how_many == expected_orders
