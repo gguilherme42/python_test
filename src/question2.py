b0e1057c3332f5e5be8a59b53b7964843aacf9c4
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2021-06-03 19:06:19 -0300

    Now, the algorithm solves the problem

--- src/question2.py
+++ src/question2.py
@@ -2,6 +2,8 @@ class Orders:
     def combine_orders(self, requests: list[int], n_max: int) -> int:
         requests_copy = requests[:]
         trips_pair = []
+
+        
         while requests_copy:
           
             highest_value = max(requests_copy)
@@ -10,19 +12,18 @@ class Orders:
             
             
             if len(requests_copy) == 1:
-                trips_pair.append((highest_value, 0))
+                trips_pair.append((requests_copy[0], 0))
+                requests_copy.remove(requests_copy[0])
+
 
             elif (highest_value + smallest_value) <= n_max:
                 trips_pair.append((highest_value, smallest_value))
-            
-            else: 
-                trips_pair += [(highest_value, 0), (smallest_value, 0)]
-            
-            if len(requests_copy) == 1:
-                requests_copy.remove(highest_value)
-            else:
                 requests_copy.remove(highest_value)
                 requests_copy.remove(smallest_value)
+            
+            else: 
+                trips_pair.append((highest_value, 0))
+                requests_copy.remove(highest_value)
             
         minimum_number_of_trips = len(trips_pair)
         
@@ -31,12 +32,11 @@ class Orders:
             
 
 
-# Tal  algoritmos não resolve essa ordem esperada   
 
 
-orders = [90, 11, 70]
+orders = [11, 90,  70, 3, 41, 55]
 n_max = 100
-expected_orders = 2
+expected_orders = 3
 
 how_many = Orders().combine_orders(orders, n_max)
 
