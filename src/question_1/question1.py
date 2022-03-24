51859e5b358397fc870f85675e22035a97491b56
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2022-03-24 00:59:13 -0300

    Improving code

--- src/question_1/question1.py
+++ src/question_1/question1.py
@@ -1,49 +1,38 @@
 class Contract:
-    def __init__(self, id, debt):
+    def __init__(self, id: int, debt: float):
         self.id = id
         self.debt = debt
 
-    def get_id(self) -> int:
-        return self.id
-    
-    def get_debt(self) -> float:
-        return self.debt
-    
-
-    def __str__(self):
-        return f"id={self.id}, debt={self.debt}"
-
 
 def get_top_N_open_contracts(open_contracts: list[Contract], renegotiated_contracts: list[int], top_n: int) -> list[int]:
+    renegotiated = set(renegotiated_contracts)
 
     def not_renegotiated(contract: Contract):
-        if contract.get_id() not in renegotiated_contracts:
-            return contract.get_debt(), contract.get_id()
+        if contract.id not in renegotiated:
+            return contract
     
+    result = filter(not_renegotiated, open_contracts)
 
-    result = list(filter(not_renegotiated, open_contracts))
+    result = sorted(result, key=lambda contract: contract.debt, reverse=True)
 
     number_of_contracts_to_return = top_n if len(result) >= top_n else len(result) 
 
-    result = sorted(result, key=lambda contract: contract.get_debt(), reverse=True)[:number_of_contracts_to_return]
-
-    return [contract.get_id() for contract in result]
+    return [contract.id for contract in result[:number_of_contracts_to_return]]
 
 
 
 
 contracts_list = [
-    Contract(1, 10),
-    Contract(2, 100),
+    Contract(1, 1),
+    Contract(2, 2),
     Contract(3, 3),
     Contract(4, 4),
-    Contract(5, 20)
+    Contract(5, 5)
 ]
-renegotiated = [3, 4]
-
+renegotiated = [3]
 top_n = 3
 
-actual_open_contracts = get_top_N_open_contracts(open_contracts=contracts_list, renegotiated_contracts=renegotiated, top_n=top_n)
+actual_open_contracts = get_top_N_open_contracts(contracts_list, renegotiated, top_n)
 
-expected_open_contracts = [2, 5, 1]
+expected_open_contracts = [5,4,2]
 assert expected_open_contracts == actual_open_contracts
