71b7052e1270245faaeb3ae9750e47dace4668e7
gguilherme42 <58306122+gguilherme42@users.noreply.github.com>
2022-03-24 01:32:15 -0300

    Improving code and fixing the solution

--- src/question_2/question2.py
+++ src/question_2/question2.py
@@ -1,25 +1,34 @@
 class Orders:
     def combine_orders(self, requests: list[int], n_max: int) -> int:
         result = sorted(requests)
-        total_trips = 0
-        while True:
-            if result:
-                high_value = max(result)
-                min_value = min(result)
+        minimum_trips = 0
+        
+        while result:    
+            if len(result) > 1:
+                high_value = result[-1]
+                min_value = result[0]
 
-                if len(result) > 1:
-                    if (high_value + min_value) <= n_max:
-                        total_trips += 1
-                        result.remove(min_value)
-                        result.remove(high_value)
-                    else:
-                        total_trips += 2
+                if (high_value + min_value) <= n_max:
+                    minimum_trips += 1
+                    result.remove(min_value)
+                    result.remove(high_value)
                 else:
-                    total_trips += 1
-                    break
+                    minimum_trips += 1
+                    result.remove(high_value) # It removes the high value because it doesn't have a pair, but the lowest could have.
+                              
             else:
+                minimum_trips += 1
                 break
-
-        return total_trips
-
+                
             
+
+        return minimum_trips
+
+
+orders = [70, 30, 10,]
+n_max = 100
+expected_orders = 2
+
+how_many = Orders().combine_orders(orders, n_max)
+
+assert how_many == expected_orders 
